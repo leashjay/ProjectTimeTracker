739b37026eb75a373fe73120f4d68fe01330b4a9
Leesh <48419288+leashjay@users.noreply.github.com>
2021-02-02 15:21:34 +1300

    Add files via upload
    Initial Commit. 
    - File implements argparser to allow -h option. 
    - Basic functionality to parse argument message with the concept of current actions can be logged to a log.txt file in current working directory
    
    Future Work.  
    - Add time tracking between logs to allow task switching and record time spent on task.  
    - Add optional set new log file location option

---
+++ time_tracker.py
@@ -0,0 +1,59 @@
+# Time Tracker
+# The helper program concept
+# for logging time on projects
+# A.Josephs 02/02/2021
+
+import argparse
+import sys
+
+from datetime import datetime
+
+
+def parse(inputs):
+    """parse arguments, currently only the message"""
+    my_parser = argparse.ArgumentParser(description='Log an activity message to file')
+
+    my_parser.add_argument('Message',
+                           metavar='message',
+                           type=str,
+                           help='the message to log on file')
+
+    my_parser.add_argument('-t',
+                       '--tag',
+                       help='add a tag to the message')
+
+    args = my_parser.parse_args()
+
+    input_message = args.Message
+
+    return input_message
+
+def write_to_file(message):
+    """save/append to file with timestamp"""
+    log_file = open("MyLog.txt","a")
+    time = get_time()
+    new_line = "{} {} \n".format(time, message)
+    log_file.write(new_line)
+    log_file.close()
+
+def get_time():
+    """Obviously get the current time but also make it pretty"""
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(dt_string)
+    return dt_string
+
+def calculate_working_time():
+    """calculate time between previous entry and new entry"""
+    print("1 hr : this is how long you have been working between logs")
+    # get previous line and parser
+    # get current time (GLOBAL)
+    # insert line with difference and string message
+
+def main(inputs):
+    message = parse(inputs)
+    write_to_file(message)
+
+
+if __name__ == "__main__":
+    main(sys.argv)
