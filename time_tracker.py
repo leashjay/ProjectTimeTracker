ba257a4b0a258beb4f8252cdb10110597935fdf8
Leesh <48419288+leashjay@users.noreply.github.com>
2021-02-02 16:44:59 +1300

    Update time_tracker.py
    Do it again, but make it a class. 
    - Added functionality for time on task.

--- time_tracker.py
+++ time_tracker.py
@@ -6,6 +6,26 @@
 import argparse
 
 from datetime import datetime
+from os import path
+
+class LogItem:
+    def __init__(self, message):
+        self.file_name = "mylog.txt"
+        self.message = message
+        self.current_time = get_time()
+        self.previous_datetime = get_last_datetime(self.file_name)
+        self.time_on_task = calculate_working_time(self)
+
+def get_last_datetime(filename):
+    if path.exists(filename) and path.getsize(filename) != 0:
+        with open(filename, "r") as file:
+            for line in file:
+                pass
+        line_p = line.split(" ")
+        return line_p[0] + " " + line_p[1]
+    else:
+        print("File ghost")
+        return None, None
 
 def parse():
     """parse arguments, currently only the message"""
@@ -22,15 +42,14 @@ def parse():
                        help='add a tag to the message')
 
     args = my_parser.parse_args()
-
     return args
 
-def write_to_file(message):
+def write_to_file(log_item):
     """save/append to file with timestamp"""
-    log_file = open("MyLog.txt","a")
-    time = get_time()
-    new_line = "{} {} \n".format(time, message)
-    log_file.write(new_line)
+    log_file = open(log_item.file_name, "a")
+    time_on_str = "-- Time on task {} --".format(log_item.time_on_task)
+    new_log = "{} {} {} \n".format(log_item.current_time, log_item.message, time_on_str)
+    log_file.write(new_log)
     log_file.close()
 
 def get_time():
@@ -39,17 +58,16 @@ def get_time():
     dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
     return dt_string
 
-def calculate_working_time():
+def calculate_working_time(self):
     """calculate time between previous entry and new entry"""
-    print("1 hr : this is how long you have been working between logs")
-    # get previous line and parser
-    # get current time (GLOBAL)
-    # insert line with difference and string message
+    c_t = datetime.strptime(self.current_time, "%d/%m/%Y %H:%M:%S")
+    p_t = datetime.strptime(self.previous_datetime, "%d/%m/%Y %H:%M:%S")
+    return c_t - p_t
 
 def main():
     args = parse()
-    input_message = args.Message
-    write_to_file(input_message)
+    new_loggable = LogItem(args.Message)
+    write_to_file(new_loggable)
 
 
 if __name__ == "__main__":
